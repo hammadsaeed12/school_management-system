6e2e57a8f6ccb76ae44d9fb7603e15eef95d820a
hammadsaeed12 <hammadsaeed@devxonic.com>
2025-02-12 17:24:55 +0500

    Add initial dashboard layout and pages with user interface components

---
+++ src/app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function TeacherPage() {
+  return (
+    <div >TeacherPage</div> )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        
       },
+      colors:{
+        lamaSky:'#C3EBFA',
+        lamaSkyLight:'#EDF9FD',
+        lamaPurple:'#CFCEFF',
+        lamaPurpleLight:'#F1F0FF',
+        lamaYellow:'#FAE27C',
+        lamaYellowLight:'#FEFCE8'
+
+      }
     },
   },
   plugins: [],
