9bcc3de6535d8406f5af94265e1014bdb32acd4b
hammadsaeed12 <hammadsaeed@devxonic.com>
2025-02-13 12:55:41 +0500

    Add Event Calendar and Announcement components; update AdminPage layout and styles

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,34 +1,44 @@
-import AttenndanceChart from '@/components/AttenndanceChart'
-import CountCharts from '@/components/CountCharts'
-import UserCard from '@/components/UserCard'
-import React from 'react'
+import Announcement from "@/components/Announcement";
+import AttenndanceChart from "@/components/AttenndanceChart";
+import CountCharts from "@/components/CountCharts";
+import EventCalender from "@/components/EventCalender";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 export default function AdminPage() {
   return (
-    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
-      {/* USER CARD */}
-      <div className='flex gap-4 justify-between flex-wrap'>
-        <UserCard type='student'/>
-        <UserCard type='teacher'/>
-        <UserCard type='parent'/>
-        <UserCard type='staff'/>
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARD */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
         {/* MIDDLE CHARTS */}
-        <div className='flex gap-4 flex-col lg:flex-row'>
+        <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
-          <div className='w-full lg:w-1/3 h-[450px]'>
-          <CountCharts/>
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountCharts />
           </div>
           {/* ATTENDANCE CHART */}
-          <div className='w-full lg:w-2/3 h-[450px]'><AttenndanceChart/></div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttenndanceChart />
+          </div>
         </div>
         {/* BOTTOM CHARTS */}
-        <div className='w-full h-[500px]'></div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* RIGHT */}
-      <div className='w-full lg:w-1/3'>R</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalender />
+        <Announcement/>
       </div>
-  )
+    </div>
+  );
 }
