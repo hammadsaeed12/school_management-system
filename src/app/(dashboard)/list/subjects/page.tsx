bfd10e2f56ab44d7fa2a7755aef48846958540c6
hammadsaeed12 <hammadsaeed@devxonic.com>
2025-02-25 14:06:36 +0500

    Fix formatting in SubjectsListPage to improve readability of role-based rendering

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -112,12 +112,12 @@ const SubjectsListPage = async ({
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && (
+            {role === "admin" && 
               // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               //   <Image src="/plus.png" alt="" width={14} height={14} />
               // </button>
               <FormModal table={"subject"} type={"create"} />
-            )}
+            }
           </div>
         </div>
       </div>
